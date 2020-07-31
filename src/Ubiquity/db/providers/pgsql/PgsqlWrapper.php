02149db3da454aa903d6fce97a91b2a3b9d69107
jcheron <myaddressmail@gmail.com>
2020-07-31 19:59:16 +0200

    add async DAO functions

--- src/Ubiquity/db/providers/pgsql/PgsqlWrapper.php
+++ src/Ubiquity/db/providers/pgsql/PgsqlWrapper.php
@@ -3,5 +3,97 @@ namespace Ubiquity\db\providers\pgsql;
 use Ubiquity\db\providers\AbstractDbWrapper;
 
 class PgsqlWrapper extends AbstractDbWrapper{
+	public function queryColumn(string $sql, int $columnNumber = null) {}
+
+	public function getDSN(string $serverName, string $port, string $dbName, string $dbType = 'mysql') {
+		$port??=5432;
+		$serverName??='127.0.0.1';
+		return "host='$serverName' port=$port dbname='$dbName'";
+	}
+
+	public function fetchAllColumn($statement, array $values = null, string $column = null) {}
+
+	public function ping() {}
+
+	public function commit() {}
+
+	public function prepareStatement(string $sql) {
+		$id=\crc32($sql);
+		\pg_prepare($this->dbInstance, $id,$sql);
+		return $id;
+	}
+
+	public function queryAll(string $sql, int $fetchStyle = null) {}
+
+	public function releasePoint($level) {}
+
+	public function lastInsertId($name = null) {}
+
+	public function nestable() {}
+
+	public static function getAvailableDrivers() {}
+
+	public function rollbackPoint($level) {}
+
+	public function getTablesName() {}
+
+	public function getStatement(string $sql) {}
+
+	public function connect(string $dbType, $dbName, $serverName, string $port, string $user, string $password, array $options) {
+		$connect_type=null;
+		$identif=" user='$user' password='$password'";
+		if($options['async']??false){
+			$connect_type=\PGSQL_CONNECT_ASYNC;
+		}
+		if($options['persistent']??false){
+			return $this->dbInstance=\pg_pconnect($this->getDSN($serverName, $port, $dbName).$identif,$connect_type);
+		}
+		return $this->dbInstance=\pg_connect($this->getDSN($serverName, $port, $dbName).$identif,$connect_type);
+	}
+
+	public function groupConcat(string $fields, string $separator): string {}
+
+	public function inTransaction() {}
+
+	public function fetchAll($statement, array $values = null, $mode = null) {}
+
+	public function query(string $sql) {}
+
+	public function fetchColumn($statement, array $values = null, int $columnNumber = null) {}
+
+	public function execute(string $sql) {}
+
+	public function fetchOne($statement, array $values = null, $mode = null) {}
+
+	public function getFieldsInfos($tableName) {}
+
+	public function bindValueFromStatement($statement, $parameter, $value) {}
+
+	public function getRowNum(string $tableName, string $pkName, string $condition): int {}
+
+	public function rollBack() {}
+
+	public function getForeignKeys($tableName, $pkName, $dbName = null) {}
+
+	public function beginTransaction() {}
+
+	public function _optPrepareAndExecute($sql, array $values = null, $one = false) {}
+	
+	public function _optExecuteAndFetch($statement, array $values = null, $one = false) {
+		$result=\pg_execute($this->dbInstance,$statement, $values);
+		if($one){
+			return \pg_fetch_row($result);
+		}
+		return \pg_fetch_all($result);
+	}
+
+	public function statementRowCount($statement) {}
+
+	public function savePoint($level) {}
+
+	public function executeStatement($statement, array $values = null) {}
+
+	public function getPrimaryKeys($tableName) {}
+
 }
 
